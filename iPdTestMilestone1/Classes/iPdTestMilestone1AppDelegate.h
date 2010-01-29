b79394b8d02e8c6c78d8da7b4be42ac50e95d07e
virusys <virusys@6e5d4a18-0d2a-11df-96d9-85c6b80b8d9c>
2010-01-29 23:09:18 +0000

    Initial Import... let's try this again
    git-svn-id: http://ipd.googlecode.com/svn@1 6e5d4a18-0d2a-11df-96d9-85c6b80b8d9c

---
+++ iPdTestMilestone1/Classes/iPdTestMilestone1AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  iPdTestMilestone1AppDelegate.h
+//  iPdTestMilestone1
+//
+//  Created by Niv on 10-01-22.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPdTestMilestone1AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ iPdTestMilestone1/Classes/iPdTestMilestone1AppDelegate.m
@@ -0,0 +1,36 @@
+//
+//  iPdTestMilestone1AppDelegate.m
+//  iPdTestMilestone1
+//
+//  Created by Niv on 10-01-22.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import "iPdTestMilestone1AppDelegate.h"
+
+int pd_main();			//cool
+
+@implementation iPdTestMilestone1AppDelegate
+
+@synthesize window;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+	//first let's generate the command line message
+	const char *path = [[[[NSBundle mainBundle] bundlePath] stringByAppendingString:@"/"] UTF8String];
+	const char *file = [[[[NSBundle mainBundle] bundlePath] 
+						 stringByAppendingString:@"/metrobang.pd"] UTF8String];
+	printf("Attempting to open %s\n", file);
+    const char *argv[] = {path, "-nogui", "-nomidi", "-noaudio", file};
+	pd_main(5, argv);			//start up pd lol
+    // Override point for customization after application launch
+    [window makeKeyAndVisible];
+}
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
